4a2dbe29f3f34c55ae092c4caefabb4bfafc2aaf
Daniel Kosieradzki <daniel.kosieradzki@bluesoft.com>
2022-10-02 12:21:32 +0200

    feat: add skeleton code for rest api

--- python/main.py
+++ python/main.py
@@ -1,34 +1,45 @@
 import serial
 import json
-import sys
+import logging
+from fastapi import FastAPI
+from model.measurement import Measurement
+from threading import Thread
 
 SERIAL_PORT_DEVICE_FILE = "/dev/tty.usbserial-14210"
-
-class Measurement:
-  def __init__(self, measurement: object) -> None:
-    self.year = int(measurement['y'])
-    self.month = int(measurement['m'])
-    self.day = int(measurement['d'])
-    self.hours = int(measurement['h'])
-    self.minutes = int(measurement['min'])
-    self.seconds = int(measurement['sec'])
-
-    self.temperature = float(measurement['t'])
-    self.humidity = float(measurement['r'])
-
-    self.pm_2_5 = int(measurement['cpm2.5'])
-    self.pm_1_0 = int(measurement['cpm1.0'])
-    self.pm_10 = int(measurement['cpm10'])
-
-    self.air_quality_index = int(measurement['aqi'])
+SERIAL_PORT_BUFFER_SIZE = 50
 
 
-  def __str__(self) -> str:
-    return f"{self.year}-{self.month:02d}-{self.day:02d} {self.hours:02d}:{self.minutes:02d}:{self.seconds:02d} - temperature: {self.temperature}, humidity: {self.humidity}, AQI: {self.air_quality_index}, PM2.5: {self.pm_2_5}"
-  
-  def to_json(self) -> str:
-    measurement_dictionary = {attribute_name: attribute_value for attribute_name, attribute_value in self.__dict__.items()}
-    return json.dumps(measurement_dictionary)
+def initialize_app() -> None:
+  logging.basicConfig(level=logging.INFO)
+
+def _start_scrapping_sensor_data() -> None:
+  sdp = SensorDataProcessor()
+
+  with serial.Serial(SERIAL_PORT_DEVICE_FILE, 115200) as serial_port:
+    # Stop realtime measurements sending mode
+    # serial_port.write(b'{"fun":"05","flag":"0"}')
+
+    # Set time interval for real-time measurements sending mode
+    # serial_port.write(b'{"fun":"01","sendtime":"001"}}')
+    # Start realtime measurements sending mode
+    serial_port.write(b'{"fun":"05","flag":"1"}')
+    # serial_port.read(100)
+
+    while True:
+      message_portion = serial_port.read(SERIAL_PORT_BUFFER_SIZE)
+      sdp.process_message_portion(message_portion)
+
+
+def start_scrapping_sensor_data() -> None:
+  Thread(
+    target=_start_scrapping_sensor_data,
+    daemon=True
+  ).start()
+
+
+def start_api() -> None:
+  global app
+  app = FastAPI()
 
 
 class SensorDataProcessor:
@@ -39,21 +50,21 @@ class SensorDataProcessor:
     self.right_curly_brace_count = 0
 
 
-  def _print_message(self, message: str):
-    message = json.loads(message)
-    if message["res"] == "4":
-      m = Measurement(message)
-      # print(m)
-      print(m.to_json())
-      # self._print_measurement_message(message)
+  def _print_message(self, message: str) -> None:
+    message_object = json.loads(message)
+    if message_object["res"] == "4":
+      m = Measurement(message_object)
+      logging.info(m.to_json())
+    else:
+      logging.info(message)
 
 
-  def process_message(self):
+  def process_message(self) -> None:
     message = self.message_buffer.decode("utf8")
     self._print_message(message)
 
 
-  def process_message_portion(self, message_portion: bytes):
+  def process_message_portion(self, message_portion: bytes) -> None:
     for byte in message_portion:
       self.message_buffer.append(byte)
       if byte == ord('}'):
@@ -74,26 +85,14 @@ class SensorDataProcessor:
       elif byte == ord('{'):
         self.left_curly_brace_count = self.left_curly_brace_count + 1
 
-    # print(f"self.left_curly_brace_count: {self.left_curly_brace_count}")
-    # print(f"self.right_curly_brace_count: {self.right_curly_brace_count}")
 
-  # print(msg)
+initialize_app()
+start_scrapping_sensor_data()
+start_api()
 
+@app.get("/health")
+def check_health():
+  #TODO: Implement some real checks like checking whether serial port has been opened successfully
+  return {"status_code": 0, "message": "OK"}
 
-if __name__ == "__main__":
-  sdp = SensorDataProcessor()
-
-  with serial.Serial(SERIAL_PORT_DEVICE_FILE, 115200) as serial_port:
-    # Stop realtime measurements sending mode
-    # serial_port.write(b'{"fun":"05","flag":"0"}')
-
-    # Set time interval for real-time measurements sending mode
-    # serial_port.write(b'{"fun":"01","sendtime":"001"}}')
-
-    # Start realtime measurements sending mode
-    serial_port.write(b'{"fun":"05","flag":"1"}')
-    # serial_port.read(100)
-
-    while True:
-      message_portion = serial_port.read(50)
-      sdp.process_message_portion(message_portion)
+# @app.get("api/v1/measurements")
---
+++ python/model/measurement.py
@@ -0,0 +1,28 @@
+import json
+
+
+class Measurement:
+  def __init__(self, measurement: object) -> None:
+    self.year = int(measurement['y'])
+    self.month = int(measurement['m'])
+    self.day = int(measurement['d'])
+    self.hours = int(measurement['h'])
+    self.minutes = int(measurement['min'])
+    self.seconds = int(measurement['sec'])
+
+    self.temperature = float(measurement['t'])
+    self.humidity = float(measurement['r'])
+
+    self.pm_2_5 = int(measurement['cpm2.5'])
+    self.pm_1_0 = int(measurement['cpm1.0'])
+    self.pm_10 = int(measurement['cpm10'])
+
+    self.air_quality_index = int(measurement['aqi'])
+
+
+  def __str__(self) -> str:
+    return f"{self.year}-{self.month:02d}-{self.day:02d} {self.hours:02d}:{self.minutes:02d}:{self.seconds:02d} - temperature: {self.temperature}, humidity: {self.humidity}, AQI: {self.air_quality_index}, PM2.5: {self.pm_2_5}"
+  
+  def to_json(self) -> str:
+    measurement_dictionary = {attribute_name: attribute_value for attribute_name, attribute_value in self.__dict__.items()}
+    return json.dumps(measurement_dictionary)
